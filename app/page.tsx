e6acf717ef22e2650b8b5bf6985a900f80dd4458
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-11-24 22:54:26 +0900

    feat: react-hook-formの導入

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <main className="max-w-[800px] w-full mx-auto mt-12">
+    <main className="mt-12">
       <h2 className="font-semibold text-2xl text-center mb-4">お問い合わせフォーム</h2>
       <MailForm />
     </main>
--- components/mail-form/mail-form.tsx
+++ components/mail-form/mail-form.tsx
@@ -1,7 +1,46 @@
+'use client'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useForm } from "react-hook-form";
+
 export default function MailForm() {
+
+  const form = useForm()
+
+  function onSubmit(values: any) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
-    <div>
-      <p>MailForm</p>
-    </div>
-  )
+    <Form {...form}>
+      <form onSubmit={() => {form.handleSubmit(onSubmit)}} className="container flex flex-col gap-3 mx-auto">
+        <FormField
+          control={form.control}
+          name="..."
+          render={(field) => (
+            <FormItem>
+              <FormLabel />
+              <FormControl>
+                <Input placeholder="shadcn" {...field}/>
+              </FormControl>
+              <FormDescription />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button>送信</Button>
+      </form>
+    </Form>
+  );
 }
