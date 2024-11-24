79c1ce53f0232497a0661e9a27a2af30d1936eb5
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-11-24 23:09:35 +0900

    feat: zodの導入

--- components/mail-form/mail-form.tsx
+++ components/mail-form/mail-form.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import {
   Form,
   FormControl,
@@ -11,30 +11,84 @@ import {
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { formSchema } from "@/lib/formSchema";
+import { Textarea } from "../ui/textarea";
 
 export default function MailForm() {
-
-  const form = useForm()
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      subject: "",
+      email: "",
+      content: "",
+    },
+  });
 
   function onSubmit(values: any) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    console.log(values);
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={() => {form.handleSubmit(onSubmit)}} className="container flex flex-col gap-3 mx-auto">
+      <form
+        onSubmit={() => {
+          form.handleSubmit(onSubmit);
+        }}
+        className="container flex flex-col gap-3 mx-auto"
+      >
         <FormField
           control={form.control}
-          name="..."
+          name="username"
           render={(field) => (
             <FormItem>
-              <FormLabel />
+              <FormLabel>ユーザー名</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field}/>
+                <Input placeholder="ユーザー名" {...field} />
               </FormControl>
               <FormDescription />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={(field) => (
+            <FormItem>
+              <FormLabel>メールアドレス</FormLabel>
+              <FormControl>
+                <Input placeholder="メールアドレス" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="subject"
+          render={(field) => (
+            <FormItem>
+              <FormLabel>主題</FormLabel>
+              <FormControl>
+                <Input placeholder="主題" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={(field) => (
+            <FormItem>
+              <FormLabel>本文</FormLabel>
+              <FormControl>
+                <Textarea placeholder="本文" {...field} className="min-h-[200px]"/>
+              </FormControl>
               <FormMessage />
             </FormItem>
           )}
